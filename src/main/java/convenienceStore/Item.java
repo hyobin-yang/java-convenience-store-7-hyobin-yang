336616f1af31a3581b3899e7fea306f9736ed5cd
양효빈 <hyobin3026@naver.com>
2024-11-09 17:44:52 +0900

    feat(Item): Add method to validate input format - Validate price is not below zero - Validate quantity is not below zero

--- src/main/java/convenienceStore/Item.java
+++ src/main/java/convenienceStore/Item.java
@@ -6,13 +6,27 @@ public class Item extends SupplyingItem {
 
     private int quantity;
 
-    public Item(String itemName, long itemPrice, int quantity) {
-        super(itemName, itemPrice);
+    public Item(String name, long price, int quantity) {
+        super(name, price);
+        validatePrice(price);
+        validateQuantity(quantity);
         this.quantity = quantity;
     }
 
     public int getQuantity() {
         return quantity;
+    }
+
+    private void validatePrice(long price){
+        if (price < 0){
+            throw new IllegalArgumentException("잘못된 가격 형식");
+        }
+    }
+
+    private void validateQuantity(int quantity){
+        if (quantity < 0){
+            throw new IllegalArgumentException("잘못된 수량 형식");
+        }
     }
 
 }
