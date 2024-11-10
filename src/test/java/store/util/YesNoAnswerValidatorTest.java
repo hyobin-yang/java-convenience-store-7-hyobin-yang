38dfa129d8828660479b75dd8407ca09593eccf4
양효빈 <hyobin3026@naver.com>
2024-11-10 18:23:44 +0900

    test(YesNoAnswerValidatorTest): Refactor test codes for YesNoAnswer

--- src/test/java/store/util/YesNoAnswerValidatorTest.java
+++ src/test/java/store/util/YesNoAnswerValidatorTest.java
@@ -1,49 +1,51 @@
-package store.validator;
+package store.util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import validator.AnswerValidator;
+import store.controller.Exceptions;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-public class AnswerValidatorTest {
+public class YesNoAnswerValidatorTest {
 
-    private final AnswerValidator answerValidator = new AnswerValidator();
+    private final YesNoAnswer answerValidator = new YesNoAnswer();
 
     @ParameterizedTest
     @DisplayName("올바른 값이 들어오면 true값을 반환한다. 공백도 허용한다.")
     @ValueSource(strings = {"Y", "N", "y", "n"})
     void shouldReturnTrueWhenAnswerHasRightFormat(String answer){
-        // when
-        boolean isRightAnswer = answerValidator.validateAnswer(answer); //TODO: 변수명 수정
-
-        // then
-        assertThat(isRightAnswer).isEqualTo(true); // TODO: 다른 비교형식 있는지 확인
+        answerValidator.validate(answer);
     }
 
     @ParameterizedTest
     @DisplayName("올바른 값이 들어오면 true값을 반환한다. 공백도 허용한다.")
     @ValueSource(strings = {"Y ", " N ", "y  ", "   n    "})
     void shouldReturnTrueWhenAnswerHasRightFormatAndBlank(String answer){
-        // when
-        boolean isRightAnswer = answerValidator.validateAnswer(answer);
-
-        // then
-        assertThat(isRightAnswer).isEqualTo(true); // TODO: 다른 비교형식 있는지 확인
+        answerValidator.validate(answer);
     }
 
     @ParameterizedTest
     @DisplayName("올바른 값이 들어오면 true값을 반환한다. 공백도 허용한다.")
     @ValueSource(strings = {"Y\n", "N\t"}) //TODO: 정규표현식 관리
     void shouldReturnTrueWhenAnswerHasRightFormatAndBlank2(String answer){
-        // when
-        boolean isRightAnswer = answerValidator.validateAnswer(answer);
+        answerValidator.validate(answer);
+    }
 
-        // then
-        assertThat(isRightAnswer).isEqualTo(true); // TODO: 다른 비교형식 있는지 확인
+    @ParameterizedTest
+    @DisplayName("올바른 값이 들어오면 true값을 반환한다. 공백도 허용한다.")
+    @ValueSource(strings = {"Y", "y"})
+    void shouldReturnTrueWhenAnswerIsPositive(String answer){
+        assertThat(answerValidator.isPositive(answer)).isTrue();
+    }
+
+    @ParameterizedTest
+    @DisplayName("올바른 값이 들어오면 true값을 반환한다. 공백도 허용한다.")
+    @ValueSource(strings = {"N", "n"})
+    void shouldReturnFalseWhenAnswerIsNegative(String answer){
+        assertThat(answerValidator.isPositive(answer)).isFalse();
     }
 
     @ParameterizedTest
@@ -51,9 +53,9 @@ public class AnswerValidatorTest {
     @ValueSource(strings = {"Yes", "No", "Y/"})
     void shouldThrowExceptionWhenAnswerHasWrongFormat(String answer){
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> answerValidator.validateAnswer(answer))
+                assertThatThrownBy(() -> answerValidator.validate(answer))
                         .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("") // TODO: 예외메시지 출력 확인
+                        .hasMessage(Exceptions.INVALID_FORMAT.getMessage()) // TODO: 예외메시지 출력 확인
         );
     }
 
