82ecae06d1f5487d5abbc7bd78cf8cecf04dceeb
양효빈 <hyobin3026@naver.com>
2024-11-11 10:04:34 +0900

    test(ApplicationTest): Add test codes to verify result

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -51,6 +51,11 @@ class ApplicationTest extends NsTest {
             run("[감자칩-2]", "N", "N");
             assertThat(output().replaceAll("\\s", "")).contains("내실돈3,000");
         }, LocalDate.of(2024, 2, 1).atStartOfDay());
+
+        assertNowTest(() -> {
+            run("[감자칩-2]", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈3,000");
+        }, LocalDate.of(2025, 2, 1).atStartOfDay());
     }
 
     @Test
@@ -59,6 +64,7 @@ class ApplicationTest extends NsTest {
             runException("[컵라면-12]", "N", "N");
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
         });
+
     }
 
     @Override
