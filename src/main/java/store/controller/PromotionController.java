5fb08244a89097dc738e063281b93a9ce02e3ffc
양효빈 <hyobin3026@naver.com>
2024-11-11 10:05:09 +0900

    feat(PromotionController): Add methods to apply promotion correctly

--- src/main/java/store/controller/PromotionController.java
+++ src/main/java/store/controller/PromotionController.java
@@ -1,89 +1,96 @@
 package store.controller;
 
 import store.convenienceStore.Item;
-import store.util.YesNoAnswerValidator;
+import store.message.Exceptions;
+import store.util.YesNoAnswer;
 
 import store.view.InputProvider;
 import store.view.InputView;
+import store.view.OutputView;
 
 public class PromotionController {
-    InputView inputView;
-    YesNoAnswerValidator validator = new YesNoAnswerValidator();
+    private final InputView inputView;
+    private final OutputView outputView = new OutputView();
+    private final YesNoAnswer validator = new YesNoAnswer();
+    private int countToBuyForPromotion;
+    private int promotionThreshold;
+    private int quantityToBuy;
 
-    public PromotionController(InputProvider inputProvider){
+    public PromotionController(InputProvider inputProvider, InputView inputView){
         this.inputView = new InputView(inputProvider);
     }
 
-    public PromotionDTO getFreeItemCount(Item generalItem, Item promotionItem, int quantityToBuy){
-        if (promotionItem == null){
-            return new PromotionDTO(quantityToBuy, 0);
+    public PromotionDTO getPromotionDTO( Item generalItem, Item promotionItem, int quantityToBuy) {
+        int totalStock = calculateTotalStock(generalItem, promotionItem);
+        this.countToBuyForPromotion = promotionItem.getPromotion().getNumberOfItemToBuy();
+        this.promotionThreshold = countToBuyForPromotion + promotionItem.getPromotion().getNumberOfItemToGet();
+        this.quantityToBuy = quantityToBuy;
+
+        if (canAddFreePromotionItem(promotionItem.getItemQuantity())) {
+            handleAdditionalItemOption(generalItem.getItemName());
         }
 
-        int updatedQuantityToBuy = quantityToBuy;
-        int generalQuantity = generalItem.getItemQuantity();
-        int promotionQuantity = promotionItem.getItemQuantity();
-        int totalQuantity = generalQuantity + promotionQuantity;
-        int countToBuyForPromotion = promotionItem.getPromotion().getNumberOfItemToBuy();
-        String itemName = generalItem.getItemName();
+        int freeItemQuantity = getFreeItemQuantity(promotionItem.getItemQuantity(), generalItem.getItemName());
+        return new PromotionDTO(generalItem, this.quantityToBuy, freeItemQuantity);
+    }
 
-        // 먼저 프로모션과 일반 재고 합친 수가 구매 수량보다 많거나 같은지 확인
-        validateEnoughQuantity(totalQuantity, quantityToBuy);
+    private int calculateTotalStock(Item generalItem, Item promotionItem) {
+        return generalItem.getItemQuantity() + promotionItem.getItemQuantity();
+    }
 
-        // 재고는 충분히 있을 때 진행
-        if (canGetOneFree(quantityToBuy, countToBuyForPromotion)){ // 무료 증정 받을 수 있을 때
-            if (getAnswerToBringMoreItem(inputView.getAnswerToBringMoreItem(itemName,1))){ // 하나 더 가져올 건지 물어본다
-                updatedQuantityToBuy += 1; // 가져오면 총 수량 증가시킨다
-                validateEnoughQuantity(totalQuantity, updatedQuantityToBuy); // 다시 수량 충분한지 확인한다 - 불충분 시 예외
-            }
+    private boolean canAddFreePromotionItem(int promotionQuantity) {
+        return ( (this.quantityToBuy + 1) % promotionThreshold == 0
+                && promotionQuantity >= this.quantityToBuy + 1);
+    }
+
+    private void handleAdditionalItemOption(String itemName) {
+        if (askCustomerToAddItem(itemName, 1)) this.quantityToBuy += 1;
+    }
+
+    private boolean askCustomerToAddItem(String itemName, int additionalQuantity) {
+        try {
+            String answer = inputView.getAnswerToBringMoreItem(itemName, additionalQuantity);
+            validator.validate(answer);
+            return validator.isPositive(answer);
+        } catch (IllegalArgumentException e) {
+            outputView.outputExceptionMessage(Exceptions.INVALID_INPUT.getMessage());
+            return askCustomerToAddItem(itemName, additionalQuantity);
         }
-
-        int freeQuantity = getQuantityPromotionApplied(updatedQuantityToBuy, promotionQuantity, countToBuyForPromotion, itemName);
-        return new PromotionDTO(updatedQuantityToBuy, freeQuantity); // 증정 개수
     }
 
-    // 전체 수량과 구매 수량 비교 - 전체 수량 부족 시 예외
-    private void validateEnoughQuantity(int totalQuantity, int quantityToBuy){
-        if (quantityToBuy > totalQuantity){
-            throw new IllegalArgumentException(Exceptions.INSUFFICIENT_STOCK_QUANTITY.getMessage());
+    private int getFreeItemQuantity(int promotionQuantity, String itemName) {
+        int freeItemQuantity = (this.quantityToBuy / (countToBuyForPromotion + 1));
+        if (hasEnoughPromotionStock(freeItemQuantity, promotionQuantity)) {
+            return freeItemQuantity;
+        }
+        return handleInsufficientPromotionStock(promotionQuantity, itemName);
+    }
+
+    private boolean hasEnoughPromotionStock(int freeItemQuantity, int promotionQuantity) {
+        return (promotionQuantity >= freeItemQuantity * promotionThreshold);
+    }
+
+    private int handleInsufficientPromotionStock(int promotionQuantity, String itemName) {
+        int freeItemQuantity = promotionQuantity / promotionThreshold;
+        int remainingQuantity = this.quantityToBuy - freeItemQuantity * promotionThreshold;
+
+        boolean willProceedWithoutPromotion = askToProceedWithoutPromotion(itemName, remainingQuantity);
+        if (!willProceedWithoutPromotion) {
+            this.quantityToBuy -= remainingQuantity;
+            freeItemQuantity = 0;
+        }
+        return freeItemQuantity;
+    }
+
+    private boolean askToProceedWithoutPromotion(String itemName, int quantity) {
+        try {
+            String answer = inputView.getAnswerToContinueBuying(itemName, quantity);
+            validator.validate(answer);
+            return validator.isPositive(answer);
+        } catch (IllegalArgumentException e) {
+            outputView.outputExceptionMessage(Exceptions.INVALID_INPUT.getMessage());
+            return askToProceedWithoutPromotion(itemName, quantity);
         }
     }
-
-    // 하나 무료로 증정받을 수 있는지 확인
-    private boolean canGetOneFree(int quantityToBuy, int countToBuyForPromotion){
-        return (quantityToBuy % (countToBuyForPromotion+1) == countToBuyForPromotion);
-    }
-
-    private int getQuantityPromotionApplied(int quantityToBuy, int promotionQuantity, int countToBuyForPromotion, String itemName){
-        // 받아야 하는 무료 증정 개수
-        int freeQuantity = (quantityToBuy / (countToBuyForPromotion+1));
-
-        // 그런데 무료 증정 개수까지 해서 프로모션 수량이 충분한가
-        if (hasEnoughPromotionQuantity(freeQuantity, promotionQuantity, countToBuyForPromotion)){
-            return freeQuantity;
-        }
-
-        // 만약 프로모션 수량 부족할 때 - 프로모션 수량에 맞춰 받을 수 있는 무료 증정 개수를 구한다
-        freeQuantity = promotionQuantity/(countToBuyForPromotion+1);
-
-        // 프로모션 적용 없이 구매해야 하는 상품 개수
-        int quantityCanNotAppliedPromotion = quantityToBuy - freeQuantity*(countToBuyForPromotion+1);
-
-        // 근데 이때는 메시지 출력해야 함 - 프로모션 적용 안 되는데도 결제할 거냐고
-        String answer = inputView.getAnswerToContinueBuying(itemName, quantityCanNotAppliedPromotion);
-        if (getAnswerToBringMoreItem(answer)){ // 구매한다 답했을 때
-            return freeQuantity; // 반환
-        }
-        throw new IllegalArgumentException(Exceptions.CANCEL_PURCHASE.getMessage()); // 구매 안 한다고 했을 때 -> 처음부터 다시 입력하셈
-    }
-
-    private boolean getAnswerToBringMoreItem(String answer){
-        validator.validateAnswer(answer);
-        return validator.isPositiveAnswer(answer);
-    }
-
-    private boolean hasEnoughPromotionQuantity(int freeQuantity, int promotionQuantity, int countToBuyForPromotion){
-        return (promotionQuantity >= freeQuantity*(countToBuyForPromotion+1));
-    }
-
 
 }
